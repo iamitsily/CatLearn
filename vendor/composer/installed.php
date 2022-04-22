0f812855ec7e317846f189022134e689ee09ac32
iamitsily <20206243@uatx.mx>
2022-04-21 23:18:51 -0500

    AdminLteOtro

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'cc12cc8991a0700fbd2e5c2d266df497be693220',
+        'reference' => '573bce47e8d86baa57de138dae2e11b73b6d5180',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -451,7 +451,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'cc12cc8991a0700fbd2e5c2d266df497be693220',
+            'reference' => '573bce47e8d86baa57de138dae2e11b73b6d5180',
             'dev_requirement' => false,
         ),
         'laravel/sail' => array(
@@ -488,6 +488,15 @@
             'install_path' => __DIR__ . '/../laravel/tinker',
             'aliases' => array(),
             'reference' => 'dff39b661e827dae6e092412f976658df82dbac5',
+            'dev_requirement' => false,
+        ),
+        'laravel/ui' => array(
+            'pretty_version' => 'v3.4.5',
+            'version' => '3.4.5.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../laravel/ui',
+            'aliases' => array(),
+            'reference' => 'f11d295de1508c5bb56206a620b00b6616de414c',
             'dev_requirement' => false,
         ),
         'league/commonmark' => array(
